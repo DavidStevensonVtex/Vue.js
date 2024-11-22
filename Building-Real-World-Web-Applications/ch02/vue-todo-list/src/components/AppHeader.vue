bb7a09cc7426f1d649ea4560316a0e06e8954c82
David Stevenson <drstevenson1958@yahoo.com>
2024-11-22 14:39:33 -0500

    Listing 2.4 The AppHeader.vue file in the vue-todo-list/src/components folder.

---
+++ Building-Real-World-Web-Applications/ch02/vue-todo-list/src/components/AppHeader.vue
@@ -0,0 +1,8 @@
+<!-- Listing 2.4 The AppHeader.vue file in the vue-todo-list/src/components folder. -->
+
+<template>
+    <header>
+        <h1><span class="icon" aria-hidden="true">✅</span></h1>
+        <p>Building Real-world Web Applications with Vue.js</p>
+    </header>
+</template>
