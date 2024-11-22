517b795cffaaf9759c08e9719a2a5f3b57660f73
David Stevenson <drstevenson1958@yahoo.com>
2024-11-22 14:21:40 -0500

    Chapter 2: Creating a Todo List App - Listing 2.1 Creating a new project
    npm init vue@latest

---
+++ Building-Real-World-Web-Applications/ch02/Listing_02-01.ps1
@@ -0,0 +1,30 @@
+# Listing 2.1 Creating a new project
+
+npm init vue@latest 
+
+# PS D:\drs\Vue.js\GitHub\Vue.js\Building-Real-World-Web-Applications\ch02> .\Listing_02-01.ps1
+
+# > npx
+# > create-vue
+
+
+# Vue.js - The Progressive JavaScript Framework
+
+# √ Project name: ... vue-todo-list
+# √ Add TypeScript? ... No / Yes
+# √ Add JSX Support? ... No / Yes
+# √ Add Vue Router for Single Page Application development? ... No / Yes
+# √ Add Pinia for state management? ... No / Yes
+# √ Add Vitest for Unit Testing? ... No / Yes
+# √ Add an End-to-End Testing Solution? » No
+# √ Add ESLint for code quality? » Yes
+# √ Add Prettier for code formatting? ... No / Yes
+
+# Scaffolding project in D:\drs\Vue.js\GitHub\Vue.js\Building-Real-World-Web-Applications\ch02\vue-todo-list...
+
+# Done. Now run:
+
+#   cd vue-todo-list
+#   npm install
+#   npm run format
+#   npm run dev
