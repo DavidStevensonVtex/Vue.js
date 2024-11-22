f0549d22b025b705fc023b8391107a155c3383f3
David Stevenson <drstevenson1958@yahoo.com>
2024-11-22 15:48:59 -0500

    Listing 2.5 Adding ListItem.vue file in the vude-todo-list/src/components folder.

---
+++ Building-Real-World-Web-Applications/ch02/vue-todo-list/src/components/ListItem.vue
@@ -0,0 +1,24 @@
+<!-- Listing 2.5 Adding ListItem.vue file in the vude-todo-list/src/components folder. -->
+
+<script lang="ts" setup>
+interface Props {
+  isChecked?: boolean | false
+}
+
+const props = defineProps<Props>()
+</script>
+<template>
+  <label :class="{ checked: props.isChecked }">
+    <input type="checkbox" :checked="props.isChecked" />
+    <slot></slot>
+  </label>
+</template>
+
+<style scoped>
+label {
+  cursor: pointer;
+}
+.checked {
+  text-decoration: line-through;
+}
+</style>
