45d726770e6e5798a8de55763dda086bdb39dd52
David Stevenson <drstevenson1958@yahoo.com>
2024-11-22 11:13:16 -0500

    Listing 1.1 My First app
    npm init vue@latest

---
+++ ch01/Listing_01-01.ps1
@@ -0,0 +1,30 @@
+# Listing 1.1 My First app
+
+npm init vue@latest
+
+# PS D:\drs\Vue.js\GitHub\Vue.js\ch01> .\Listing_01-01.ps1
+
+# > npx
+# > create-vue
+
+
+# Vue.js - The Progressive JavaScript Framework
+
+# √ Project name: ... my-first-vue
+# √ Add TypeScript? ... No / Yes
+# √ Add JSX Support? ... No / Yes
+# √ Add Vue Router for Single Page Application development? ... No / Yes
+# √ Add Pinia for state management? ... No / Yes
+# √ Add Vitest for Unit Testing? ... No / Yes
+# √ Add an End-to-End Testing Solution? » No
+# √ Add ESLint for code quality? » Yes
+# √ Add Prettier for code formatting? ... No / Yes
+
+# Scaffolding project in D:\drs\Vue.js\GitHub\Vue.js\ch01\my-first-vue...
+
+# Done. Now run:
+
+#   cd my-first-vue
+#   npm install
+#   npm run format
+#   npm run dev
