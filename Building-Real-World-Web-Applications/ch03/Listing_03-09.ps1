1ad0f0b996c66e3c24384b55b3444ffa7342a544
David Stevenson <drstevenson1958@yahoo.com>
2024-11-25 13:22:28 -0500

    Listing 3.9 Installing Tailwind CSS
    npm install -D tailwindcss postcss autoprefixer
    
    npx tailwindcss init -p

---
+++ Building-Real-World-Web-Applications/ch03/Listing_03-09.ps1
@@ -0,0 +1,8 @@
+# Listing 3.9 Installing Tailwind CSS
+
+npm install -D tailwindcss postcss autoprefixer
+
+npx tailwindcss init -p
+
+# Created Tailwind CSS config file: tailwind.config.js
+# Created PostCSS config file: postcss.config.js   
