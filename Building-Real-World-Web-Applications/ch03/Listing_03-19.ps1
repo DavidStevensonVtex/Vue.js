5a6c2f7f813900f16517b6465c1e5250757566e0
David Stevenson <drstevenson1958@yahoo.com>
2024-11-25 15:00:09 -0500

    Listing 3.19 Running the unit test

---
+++ Building-Real-World-Web-Applications/ch03/Listing_03-19.ps1
@@ -0,0 +1,24 @@
+# Listing 3.19 Running the unit test
+
+npm run test:unit
+
+# PS D:\drs\Vue.js\GitHub\Vue.js\Building-Real-World-Web-Applications\ch03\vue-local-weather> ..\Listing_03-19.ps1
+
+# > vue-local-weather@0.0.0 test:unit
+# > vitest
+
+#  DEV  v2.1.5 D:/drs/Vue.js/GitHub/Vue.js/Building-Real-World-Web-Applications/ch03/vue-local-weather
+
+# warn - The `purge`/`content` options have changed in Tailwind CSS v3.0.
+# warn - Update your configuration file to eliminate this warning.
+# warn - https://tailwindcss.com/docs/upgrade-guide#configure-content-sources
+#  ✓ src/App.spec.ts (1)
+#  ✓ src/components/__tests__/HelloWorld.spec.ts (1)
+
+#  Test Files  2 passed (2)
+#       Tests  2 passed (2)
+#    Start at  14:52:58
+#    Duration  19.34s (transform 1.45s, setup 0ms, collect 2.39s, tests 60ms, environment 33.73s, prepare 1.68s)
+
+#  PASS  Waiting for file changes...
+#        press h to show help, press q to quit
