4d52543aa69f49a5d79e58572c02a9cb0dd9cc0b
David Stevenson <drstevenson1958@yahoo.com>
2024-11-26 14:03:58 -0500

    Listing 4.11 The LoadingIndicator.vue file in the vue-marvel-explorer/src/components folder.

---
+++ Building-Real-World-Web-Applications/ch04/vue-marvel-explorer/src/components/LoadingIndicator.vue
@@ -0,0 +1,21 @@
+<!-- Listing 4.11 The LoadingIndicator.vue file in the vue-marvel-explorer/src/components folder. -->
+
+<script setup lang="ts">
+const props = defineProps<{
+  text?: string;
+}>();
+</script>
+<template>
+  <div
+    class="flex flex-col items-center justify-center p-4 pt-16 min-h-min min-w-screen"
+  >
+    <div v-if="text" class="mb-4">
+      {{ text }}
+    </div>
+    <div class="flex space-x-2 animate-pulse">
+      <div class="w-3 h-3 bg-gray-500 rounded-full"></div>
+      <div class="w-3 h-3 bg-gray-500 rounded-full"></div>
+      <div class="w-3 h-3 bg-gray-500 rounded-full"></div>
+    </div>
+  </div>
+</template>
