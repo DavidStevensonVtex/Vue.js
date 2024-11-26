83203489283cafbfea0f9a31bf8a1d0b0b2070ff
David Stevenson <drstevenson1958@yahoo.com>
2024-11-26 12:20:23 -0500

    Listing 4.9 The marvelApi.ts file in the vue-marvel-explorer/src/composables folder

---
+++ Building-Real-World-Web-Applications/ch04/vue-marvel-explorer/src/composables/marvelApi.ts
@@ -0,0 +1,16 @@
+// Listing 4.9 The marvelApi.ts file in the vue-marvel-explorer/src/composables folder
+
+import type { Comics } from "@/types/marvel"
+
+export const useComics = async (): Promise<Comics> => {
+    const apiKey = import.meta.env.VITE_APP_MARVEL_API_PUBLIC;
+    const MARVEL_API = `//gateway.marvel.com/v1/public/`
+    const API_SIGN = `apikey=${apiKey}`
+
+    const requestURI = `${MARVEL_API}/comics?${API_SIGN}`
+
+    const res = await fetch(requestURI)
+    const jsonRes = await res.json()
+
+    return jsonRes.data
+}
