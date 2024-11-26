68a39739b21e5a3e300dd0da0a6cbbb7b3efd9f5
David Stevenson <drstevenson1958@yahoo.com>
2024-11-26 15:17:16 -0500

    Listing 4.17 The SearchView.vue file in the vue-marvel-explorer/src/views folder

---
+++ Building-Real-World-Web-Applications/ch04/vue-marvel-explorer/src/views/SearchView.vue
@@ -0,0 +1,7 @@
+<!-- Listing 4.17 The SearchView.vue file in the vue-marvel-explorer/src/views folder -->
+
+<template>
+  <div class="search">
+    <h1>This is a search page</h1>
+  </div>
+</template>
