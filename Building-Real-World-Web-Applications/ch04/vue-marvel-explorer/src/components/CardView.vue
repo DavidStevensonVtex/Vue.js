379eee875f182b135679716ab87ae6f55d902d96
David Stevenson <drstevenson1958@yahoo.com>
2024-11-26 14:14:10 -0500

    Listing 4.13 The CardView.vue file in the vue-marvel-explorer/src/components folder.

---
+++ Building-Real-World-Web-Applications/ch04/vue-marvel-explorer/src/components/CardView.vue
@@ -0,0 +1,12 @@
+<!-- Listing 4.13 The CardView.vue file in the vue-marvel-explorer/src/components folder. -->
+
+<template>
+  <article
+    class="p-4 bg-white rounded-lg shadow-xl place-content-center text-slate-800"
+  >
+    <header>
+      <h1 class="pb-5 text-lg font-semibold"><slot name="header"></slot></h1>
+    </header>
+    <slot></slot>
+  </article>
+</template>
