1ea8cd457b134f9399b16cd8fd8195a5bebcdde1
David Stevenson <drstevenson1958@yahoo.com>
2024-11-26 11:51:45 -0500

    Listing 4.3 Installing Tailwind CSS, PostCSS, and AutoPrefixer
    pnpm install -D tailwindcss postcss autoprefixer
    pnpm dlx tailwindcss init -p

--- Building-Real-World-Web-Applications/ch04/Listing_04-03.ps1
+++ Building-Real-World-Web-Applications/ch04/Listing_04-03.ps1
@@ -1,4 +1,8 @@
-# Listing 4.2 Installing Tailwind CSS, PostCSS, and AutoPrefixer
+# Listing 4.3 Installing Tailwind CSS, PostCSS, and AutoPrefixer
 
 pnpm install -D tailwindcss postcss autoprefixer 
 pnpm dlx tailwindcss init -p 
+
+
+# Created Tailwind CSS config file: tailwind.config.js
+# Created PostCSS config file: postcss.config.js
