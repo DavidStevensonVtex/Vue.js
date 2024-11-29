98a7a11853bee6339d6dc346bce13c10fd8850c8
David Stevenson <drstevenson1958@yahoo.com>
2024-11-29 11:41:28 -0500

    Listing 4.36 The ErrorMessage.vue file in the vue-marvel-explorer/src/explorer folder

--- Building-Real-World-Web-Applications/ch04/vue-marvel-explorer/src/components/ErrorMessage.vue
+++ Building-Real-World-Web-Applications/ch04/vue-marvel-explorer/src/components/ErrorMessage.vue
@@ -5,7 +5,7 @@
     class="p-4 mx-4 my-24 bg-white rounded-lg shadow-xl place-content-center text-slate-800"
   >
     <header>
-      <h1 class="pb-5 text-lg font-semibold">Oops! 🫢</h1>
+      <h1 class="pb-5 text-lg font-semibold">Oops!</h1>
     </header>
     Something went wrong!
   </article>
