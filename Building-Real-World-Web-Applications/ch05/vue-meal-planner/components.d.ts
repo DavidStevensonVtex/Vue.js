f5951ebf512fd25b530cfe27366b422affca777c
David Stevenson <drstevenson1958@yahoo.com>
2024-11-30 15:54:00 -0500

    Listing 5.4 The CalendarDays.vue file in the vue-meal-planner/src/components folder

--- Building-Real-World-Web-Applications/ch05/vue-meal-planner/components.d.ts
+++ Building-Real-World-Web-Applications/ch05/vue-meal-planner/components.d.ts
@@ -7,6 +7,7 @@ export {}
 /* prettier-ignore */
 declare module 'vue' {
   export interface GlobalComponents {
+    CalendarDays: typeof import('./src/components/CalendarDays.vue')['default']
     HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
   }
 }
---
+++ Building-Real-World-Web-Applications/ch05/vue-meal-planner/src/components/CalendarDays.vue
@@ -0,0 +1,18 @@
+<!-- Listing 5.4 The CalendarDays.vue file in the vue-meal-planner/src/components folder -->
+
+<template>
+  <v-table>
+    <thead>
+      <tr>
+        <th class="text-left">Upcoming days</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="card in cards" :key="card.date.toString()">
+        <td class="py-4">
+          {{ card.content }}
+        </td>
+      </tr>
+    </tbody>
+  </v-table>
+</template>
