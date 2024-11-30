061b42416822555973075dd0333fe9b9cfdee67f
David Stevenson <drstevenson1958@yahoo.com>
2024-11-30 15:28:26 -0500

    Chapter 5: Building a Recipe App with Vuetify - Listing 5.1 Creating a new Vue project
    npm create vuetify

---
+++ Building-Real-World-Web-Applications/ch05/Listing_05-01.ps1
@@ -0,0 +1,13 @@
+# Listing 5.1 Creating a new Vue project
+
+npm create vuetify
+
+
+# PS D:\drs\Vue.js\GitHub\Vue.js\Building-Real-World-Web-Applications\ch05> .\Listing_05-01.ps1
+# Need to install the following packages:
+# create-vuetify@2.3.0
+# Ok to proceed? (y) y
+
+# Discord community: https://community.vuetifyjs.com
+# Github: https://github.com/vuetifyjs/vuetify
+# Support Vuetify: https://github.com/sponsors/johnleider
