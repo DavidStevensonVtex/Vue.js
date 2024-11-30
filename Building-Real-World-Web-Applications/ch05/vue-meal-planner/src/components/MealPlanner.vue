ff0ea0ff1522eb91c91992a4a59f3245daa63b07
David Stevenson <drstevenson1958@yahoo.com>
2024-11-30 16:23:02 -0500

    Chapter 5: Building a Recipe App with Vuetify
    Restart due to missing layouts and router folders.

--- Building-Real-World-Web-Applications/ch05/vue-meal-planner/src/components/MealPlanner.vue
+++
@@ -1,9 +0,0 @@
-<!-- Listing 5.5 The MealPlanner.vue file in the vue-meal-planner/src/components folder -->
-
-<script setup lang="ts">
-import CalendarDays from "./CalendarDays.vue";
-</script>
-
-<template>
-  <calendar-days :date="new Date()" :days="7" />
-</template>
