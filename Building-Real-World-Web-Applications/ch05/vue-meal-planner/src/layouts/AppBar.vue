2230591c3783655ec0721e4fba58bd2226af3def
David Stevenson <drstevenson1958@yahoo.com>
2024-11-30 17:01:25 -0500

    Listing 5.9 The AppBar.vue file in the vue-meal-planner/src/layouts folder

---
+++ Building-Real-World-Web-Applications/ch05/vue-meal-planner/src/layouts/AppBar.vue
@@ -0,0 +1,8 @@
+<!-- Listing 5.9 The AppBar.vue file in the vue-meal-planner/src/layouts folder -->
+
+<template>
+  <v-app-bar-title>
+    <v-icon icon="mdi-silverware-fork-knife" />
+    Meal Planner
+  </v-app-bar-title>
+</template>
