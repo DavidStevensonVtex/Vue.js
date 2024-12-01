15cdfe35d5f35ffba807bf3bebe0ba45aae3f748
David Stevenson <drstevenson1958@yahoo.com>
2024-11-30 20:52:14 -0500

    Listing 5.15 The RecipesView.vue file in the vue-meal-planner/src/components folder

---
+++ Building-Real-World-Web-Applications/ch05/vue-meal-planner/src/views/RecipeView.vue
@@ -0,0 +1,9 @@
+<!-- Listing 5.15 The RecipesView.vue file in the vue-meal-planner/src/components folder -->
+
+<script setup lang="ts">
+import RecipesList from "@/components/RecipesList.vue";
+</script>
+
+<template>
+  <RecipesList />
+</template>
