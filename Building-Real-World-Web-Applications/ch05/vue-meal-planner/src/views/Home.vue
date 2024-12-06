3eb0bfb69cdba6e6af8231b0cba6f828da4b9d5f
David Stevenson <drstevenson1958@yahoo.com>
2024-12-06 11:37:09 -0500

    Listing 5.17 The Home.vue file in the vue-meal-planner/src/views folder

---
+++ Building-Real-World-Web-Applications/ch05/vue-meal-planner/src/views/Home.vue
@@ -0,0 +1,9 @@
+// Listing 5.17 The Home.vue file in the vue-meal-planner/src/views folder
+
+<script lang="ts" setup>
+import MealPlanner from "@/components/MealPlanner.vue";
+</script>
+
+<template>
+  <meal-planner />
+</template>
