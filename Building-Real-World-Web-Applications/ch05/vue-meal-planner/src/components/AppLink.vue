3cb671f55c198be87df65513d86e17972ab332e1
David Stevenson <drstevenson1958@yahoo.com>
2024-12-06 12:15:25 -0500

    Listing 5.20 The AppLink.vue file in the vue-meal-planner/src/components folder

---
+++ Building-Real-World-Web-Applications/ch05/vue-meal-planner/src/components/AppLink.vue
@@ -0,0 +1,15 @@
+<!-- Listing 5.20 The AppLink.vue file in the vue-meal-planner/src/components folder -->
+
+<script setup lang="ts">
+const props = defineProps({
+  to: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+<template>
+  <router-link :to="to" class="text-light-blue">
+    <slot></slot>
+  </router-link>
+</template>
