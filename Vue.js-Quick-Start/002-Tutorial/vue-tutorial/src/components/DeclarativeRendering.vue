3d7be10a0325ef6da92547b57d5552cac4fb6a4c
David Stevenson <drstevenson1958@yahoo.com>
2024-12-09 10:24:51 -0500

    Listing 1.5 Declarative Rendering using reactive and ref functions

---
+++ Vue.js-Quick-Start/002-Tutorial/vue-tutorial/src/components/DeclarativeRendering.vue
@@ -0,0 +1,24 @@
+<!-- Listing 1.5 Declarative Rendering using reactive and ref functions -->
+
+<script setup>
+import { reactive } from "vue";
+import { ref } from "vue";
+
+const counter = reactive({
+  count: 0,
+});
+
+console.log(counter.count); // 0
+counter.count++;
+
+const message = ref("Hello World!");
+
+console.log(message.value); // "Hello World!"
+message.value = "Changed";
+</script>
+
+<template>
+  <h1>{{ message }}</h1>
+  <h1>{{ message.split("").reverse().join("") }}</h1>
+  <p>Count is: {{ counter.count }}</p>
+</template>
