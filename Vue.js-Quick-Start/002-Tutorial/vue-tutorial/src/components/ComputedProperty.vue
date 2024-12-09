1ac1e76a7a8746145e8c1d3f88110df1c8d0881a
David Stevenson <drstevenson1958@yahoo.com>
2024-12-09 11:51:41 -0500

    Listing 1.18 The ComputedProperty.vue file in the vue-tutorial/src/components folder

---
+++ Vue.js-Quick-Start/002-Tutorial/vue-tutorial/src/components/ComputedProperty.vue
@@ -0,0 +1,55 @@
+<!-- Listing 1.18 The ComputedProperty.vue file in the vue-tutorial/src/components folder -->
+
+<script setup>
+import { ref, computed } from "vue";
+
+let id = 0;
+
+const newTodo = ref("");
+const hideCompleted = ref(false);
+const todos = ref([
+  { id: id++, text: "Learn HTML", done: true },
+  { id: id++, text: "Learn JavaScript", done: true },
+  { id: id++, text: "Learn Vue", done: false },
+]);
+
+function addTodo() {
+  console.log("newTodo", newTodo.value);
+  const todo = { id: id++, text: newTodo.value };
+  todos.value.push(todo);
+  newTodo.value = "";
+}
+
+function removeTodo(todo) {
+  todos.value = todos.value.filter((t) => t.id !== todo.id);
+}
+
+const filteredTodos = computed(() => {
+  return hideCompleted.value
+    ? todos.value.filter((todo) => !todo.done)
+    : todos.value;
+});
+</script>
+
+<template>
+  <form @submit.prevent="addTodo">
+    <input v-model="newTodo" required placeholder="new todo" />
+    <button>Add Todo</button>
+  </form>
+  <ul>
+    <li v-for="todo in filteredTodos" :key="todo.id">
+      <input type="checkbox" v-model="todo.done" />
+      <span :class="{ done: todo.done }">{{ todo.text }}</span>
+      <button @click="removeTodo(todo)">X</button>
+    </li>
+  </ul>
+  <button @click="hideCompleted = !hideCompleted">
+    {{ hideCompleted ? "Show all" : "Hide completed" }}
+  </button>
+</template>
+
+<style>
+.done {
+  text-decoration: line-through;
+}
+</style>
